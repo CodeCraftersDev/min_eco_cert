879c2da9dff88b384a6209958a7669c4d8aed03d
Gustavo Ochoa <gustavoeochoa@gmail.com>
2024-02-17 16:45:52 -0300

    primer commit pagina web

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -7,13 +7,6 @@ use App\Models\TestModel;
 class Home extends BaseController
 {
     public function index(): string {
-        /*$data = array(
-            'lorem' => 'ipsum',
-            'changos' => 'locos'
-        );
-        pre($data);*/
-        $model = new TestModel();
-        $result = $model->getTest();
-        return view('welcome_message', $result);
+        return view('home_page');
     }
 }
---
+++ public/assets/js/scripts.js
@@ -0,0 +1,12 @@
+"use strict";
+
+// Preloader
+$(window).on('load', function() {
+    $("#status").delay(700).fadeOut();
+    $("#preloader").delay(700).fadeOut("slow");
+});
+
+// on ready function
+$(document).ready(function($) {
+    
+});
