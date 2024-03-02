93214a8f5c8e5246694ff8342f5ce03e1efb30c2
Gustavo Ochoa <gustavoeochoa@gmail.com>
2024-03-02 19:47:48 -0300

    certificados 2

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -18,21 +18,24 @@ class Home extends BaseController
             $model = new SumarioModel();
             $arr = $model->searchSumaryByDNI($dni);
             if(count($arr) == 0){/** no se encontro dni mandarlo a no registrado */
+                $hash = $model->generarCertificado($dni, $arr, true);
                 $resp = [
+                    'url' => base_url('certificado/'.$hash[0]['hash']),
                     'message' => 'No se ha encontrado registrado su DNI en Sumarios',
-                    'url' => base_url('certificado'),
                     'resultado' => 'OK'
                 ];
             }
             else{
+                $hash = $model->generarCertificado($dni, $arr, false);
                 $resp = [
-                    'ids'
-                    'message' => 'Se ha encontrado registrado su DNI en Sumarios',
+                    'hash' => $hash,
                     'url' => base_url('certificadoregistra'),
+                    'message' => 'Se ha encontrado registrado su DNI en Sumarios',
                     'resultado' => 'OK'
                 ];
             }
-            return $this->response->setJSON($arr)->setStatusCode(200);
+
+            return $this->response->setJSON($resp)->setStatusCode(200);
         }
         catch (\Exception $e) {
             $resp = [
