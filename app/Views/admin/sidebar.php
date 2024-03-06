5f714017334bc0f884ac2f00fab6b00b86b82d1a
Gustavo Ochoa <gustavoeochoa@gmail.com>
2024-03-06 13:07:05 -0300

    Update sidebar.php

--- app/Views/admin/sidebar.php
+++ app/Views/admin/sidebar.php
@@ -3,7 +3,7 @@
     <!-- Brand Logo -->
     <a href="<?= base_url('admin') ?>" class="brand-link bg-dark">
         <img src="<?=base_url('assets/img/')?>jaguarete.png" alt="Logo" class="brand-image img-circle elevation-3"
-             style="opacity: .9;  max-height: 40px; background-color: #106951;">
+             style="opacity: .9;  max-height: 40px; filter: invert(1);">
         <span class="brand-text font-weight-light">Min. Ecología</span>
     </a>
 
